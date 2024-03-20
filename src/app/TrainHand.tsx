b17cee0a1ea1ac60d29e1e7d85388b432acf0253
unknown <zsequeira9@gmail.com>
2024-03-19 22:40:43 -0400

    no trains display over traincard

--- src/app/TrainHand.tsx
+++ src/app/TrainHand.tsx
@@ -14,8 +14,9 @@ export default function TrainHand({ trainHand }: TrainHandProps) {
             listTrainCards.push(
                 <li key={key + i}>
                     <Draggable> 
-                        <div>
-                          <img className="train-card-hand" src={`/cards/${keyColor}.svg?url`} alt={keyColor} />
+                        <div style={{position: "relative"}}>
+                            <img style={{position: "relative"}} className="train-card-hand" src={`/cards/${keyColor}.svg?url`} alt={keyColor} />
+                            <span style={{position: "absolute", top: "0", left: "50%", border: "1px solid black", borderRadius: "25%", padding: ".5em", background: "white"}}>4</span>
                         </div> 
                     </Draggable>
                 </li>
@@ -25,6 +26,20 @@ export default function TrainHand({ trainHand }: TrainHandProps) {
     }
 
     return (
-        <ul className="train-hand list">{listTrainCards}</ul>
+        <div>
+            {/* preload images. whatever. */}
+            <div style={{display: 'none'}}>
+                    <img src={'/cards/black.svg?url'} />
+                    <img src={'/cards/blue.svg?url'} />
+                    <img src={'/cards/green.svg?url'} />
+                    <img src={'/cards/loco.svg?url'} />
+                    <img src={'/cards/orange.svg?url'} />
+                    <img src={'/cards/pink.svg?url'} />
+                    <img src={'/cards/red.svg?url'} />
+                    <img src={'/cards/white.svg?url'} />
+                </div>
+            <ul className="train-hand list">{listTrainCards}</ul>
+        </div>
+
     );
 }
