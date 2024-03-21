1b7d85591576f2f565188d9f5979f2df3cacc8d4
unknown <zsequeira9@gmail.com>
2024-03-21 09:09:58 -0400

    get rect under card

--- src/app/TrainHand.tsx
+++ src/app/TrainHand.tsx
@@ -1,43 +1,58 @@
-import Draggable from 'react-draggable';
+import Draggable, {DraggableEvent} from 'react-draggable';
 import { cardColor } from '../../interfaces'
 
 interface TrainHandProps {
     trainHand: Record<cardColor, number>;
 }
 
+function handleStop(e: DraggableEvent) {
+    // card should reset position here.. 
+    const event = e as MouseEvent;
+    console.log(event);
+    // maybe its easier to put separate mouseover event on rect..?
+    const elements = document.elementsFromPoint(event.clientX, event.clientY);
+    elements.forEach((elt) => {
+        if (elt.nodeName == 'rect') {
+            const parentElement = elt.parentElement;
+            if (parentElement !== null) {
+                console.log(parentElement.id);
+            }
+        }
+    });
+}
+
 export default function TrainHand({ trainHand }: TrainHandProps) {
 
-    const listTrainCards = [];
-    for (const key in trainHand) {
-        const keyColor = key as cardColor;
-        for (let i = 0; i < trainHand[keyColor]; i++) {
-            listTrainCards.push(
-                <li key={key + i}>
-                    <Draggable> 
-                        <div style={{position: "relative"}}>
-                            <img style={{position: "relative"}} className="train-card-hand" src={`/cards/${keyColor}.svg?url`} alt={keyColor} />
-                            <span style={{position: "absolute", top: "0", left: "50%", border: "1px solid black", borderRadius: "25%", padding: ".5em", background: "white"}}>4</span>
-                        </div> 
-                    </Draggable>
-                </li>
-            )
+    const listTrainCards = Object.keys(trainHand).map((key) => {
+        let color = key as cardColor;
+        if ( trainHand[color] >0 ) {
+            return <li key={color}>
+            <Draggable defaultPosition={{x: 0, y: 0}} onStop={handleStop}>
+                <div>
+                    {/* TODO: dragging around this images causes weird mouseup problems! */}
+                    <img className="train-card-hand" src={`/cards/${color}.svg?url`} alt={color} />
+                    <span style={{ position: "absolute", top: "0", left: "50%", border: "1px solid black", borderRadius: "25%", padding: ".5em", background: "white" }}>
+                        {trainHand[color]}
+                    </span>
+                </div>
+            </Draggable>
+        </li>
         }
-
-    }
+    });
 
     return (
         <div>
-            {/* preload images. whatever. */}
-            <div style={{display: 'none'}}>
-                    <img src={'/cards/black.svg?url'} />
-                    <img src={'/cards/blue.svg?url'} />
-                    <img src={'/cards/green.svg?url'} />
-                    <img src={'/cards/loco.svg?url'} />
-                    <img src={'/cards/orange.svg?url'} />
-                    <img src={'/cards/pink.svg?url'} />
-                    <img src={'/cards/red.svg?url'} />
-                    <img src={'/cards/white.svg?url'} />
-                </div>
+            {/* TODO: preload images in more intelligent way */}
+            <div style={{ display: 'none' }}>
+                <img src={'/cards/black.svg?url'} />
+                <img src={'/cards/blue.svg?url'} />
+                <img src={'/cards/green.svg?url'} />
+                <img src={'/cards/loco.svg?url'} />
+                <img src={'/cards/orange.svg?url'} />
+                <img src={'/cards/pink.svg?url'} />
+                <img src={'/cards/red.svg?url'} />
+                <img src={'/cards/white.svg?url'} />
+            </div>
             <ul className="train-hand list">{listTrainCards}</ul>
         </div>
 
