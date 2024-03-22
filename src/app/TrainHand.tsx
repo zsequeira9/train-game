29d63e2eda7e6311bf11ed5a64acbb8784af0c86
unknown <zsequeira9@gmail.com>
2024-03-22 09:31:51 -0400

    event handlers for draggable...

--- src/app/TrainHand.tsx
+++ src/app/TrainHand.tsx
@@ -5,29 +5,59 @@ interface TrainHandProps {
     trainHand: Record<cardColor, number>;
 }
 
-function handleStop(e: DraggableEvent) {
-    // card should reset position here.. 
+export default function TrainHand({ trainHand }: TrainHandProps) {
+    let hovered: HTMLElement | null = null;
+
+    /**
+     * Select the current route to claim
+     * @param e Draggable Event
+     */
+    function handleStop(e: DraggableEvent) {
+        // card should reset position here.. 
+        const event = e as MouseEvent;
+        const elements = document.elementsFromPoint(event.clientX, event.clientY);
+        elements.forEach((elt) => {
+            if (elt.nodeName == 'rect') {
+                const parentElement = elt.parentElement;
+                if (parentElement !== null) {
+                    hovered?.classList.remove("hovered");
+                    hovered = null;
+                    let img = e.target as HTMLImageElement;
+                    let img_alt = img.alt as cardColor;
+                    console.log(img.alt, trainHand[img_alt]);
+                    console.log(parentElement.id);
+                    // send(parentElement.id)
+                }
+           }
+        });
+    }
+
+/**
+ * Add hovered class to moused over routes
+ * @param e Draggable Event
+ */
+function handleDrag(e: DraggableEvent) {
     const event = e as MouseEvent;
-    console.log(event);
-    // maybe its easier to put separate mouseover event on rect..?
     const elements = document.elementsFromPoint(event.clientX, event.clientY);
     elements.forEach((elt) => {
         if (elt.nodeName == 'rect') {
             const parentElement = elt.parentElement;
             if (parentElement !== null) {
-                console.log(parentElement.id);
+                if (parentElement !== hovered) {
+                    hovered?.classList.remove("hovered");
+                    hovered = parentElement;
+                    hovered.classList.add("hovered");
+                }
             }
         }
     });
 }
 
-export default function TrainHand({ trainHand }: TrainHandProps) {
-
     const listTrainCards = Object.keys(trainHand).map((key) => {
         let color = key as cardColor;
         if ( trainHand[color] >0 ) {
             return <li key={color}>
-            <Draggable defaultPosition={{x: 0, y: 0}} onStop={handleStop}>
+            <Draggable defaultPosition={{x: 0, y: 0}} onStop={handleStop} onDrag={handleDrag}>
                 <div>
                     <img draggable={false} className="train-card-hand" src={`/cards/${color}.svg?url`} alt={color} />
                     <span style={{ position: "absolute", top: "0", left: "50%", border: "1px solid black", borderRadius: "25%", padding: ".5em", background: "white" }}>
