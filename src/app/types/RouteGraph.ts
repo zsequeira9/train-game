802f33fbf5c1ee785f24c736b50322197c022a86
unknown <zsequeira9@gmail.com>
2024-04-15 09:41:33 -0400

    example algo run

--- src/app/types/RouteGraph.ts
+++ src/app/types/RouteGraph.ts
@@ -5,13 +5,19 @@ interface edge {
     length: number,
 }
 
-interface node {
+interface graphNode {
     name: string,
     edges: edge[],
 }
 
+interface treeNode {
+    name: string,
+    edges: edge[],
+    parent: number,
+}
+
 export class RouteGraph {
-    graph: node[] = [];
+    graph: graphNode[] = [];
 
     /**
      * Add an edge between two cities in the graph
@@ -27,14 +33,14 @@ export class RouteGraph {
             const city1Node = {
                 name: route.city1,
                 edges: [],
-            } as node
+            } as graphNode
             city1Idx = this.graph.push(city1Node) - 1;
         }
         if (city2Idx === -1) {
             const city2Node = {
                 name: route.city2,
                 edges: [],
-            } as node
+            } as graphNode
             city2Idx = this.graph.push(city2Node) - 1;
         }
         
@@ -50,47 +56,41 @@ export class RouteGraph {
         let length = 0 as number;
         let path = [] as string[];
 
-        let stack = [] as node[];
-
         // TODO: pick a starting node with high degree
-        let startingNode = 0 as number;
-        stack.push(this.graph[startingNode]);
+        let startingIdx = this.graph.findIndex((node) => node.edges.length > 2);
+        startingIdx = startingIdx === -1 ? 0 : startingIdx;
 
-        // let currentLeg = [] as node[];
+        let root = {parent: startingIdx, ...this.graph[startingIdx]} as treeNode
 
-        while (stack.length !== 0) {
-            // get next node in stack
-            let node = stack.shift();
-            if (node !== undefined) {
-                
-            //     if (!node.visited) {
-            //         node.visited = true;
-            //         if (node.parent === currentLeg[0].name) {
-            //             // 
-            //         }
-            //         // terminal node
-            //         if (node.edges.length == 1 && this.graph[node.edges[0].to].name === currentLeg[currentLeg.length-1].name) {
-            //             currentLeg.push(node);
-            //         }
-            //         else {
-            //             for (const edge of node.edges) {
-            //                 // push all new nodes onto the stack
-            //                 if (this.graph[edge.to].name !== currentLeg[currentLeg.length-1].name) {
-            //                     this.graph[edge.to].parent = node.name;
-            //                     stack.push(this.graph[edge.to])
-            //                 }
-            //             }
-            //             currentLeg.push(node);
-            //         }
-            //     }
-            //     // node has already been visited
-            //     else {
-            //     }
-            }
-
-        }
-
-
+        /**
+         * 0: node = null, stack = [zurich], currentPath = []
+         * 1: node = zurich, stack = [paris, charseille, vienna], currentPath = [zurich]
+         * 2: paris, [bruxelles, charseille, vienna], [zurich, paris]
+         * 3: bruxelles, [charseille, vienna], [zurich, paris:5, bruxelles:2] // calc scores if len(edges) == 1
+         * 4: charseille, [barcelona, vienna], [zurich, paris:5, bruxelles:2], [charseille] // if node.parent !== currentPath[length-1]{prevPath = currentPath, currentPath=[node.parent, node]}
+         * 5: barcelona, [madrid, vienna], [zurich, paris:5, bruxelles:2], [zurich, charseille, barcelona]
+         * 6: madrid, [vienna], [zurich, paris:5, bruxelles:2], [zurich, charseille:8, barcelona:6, madrid:2]
+         * 7: vienna, [zagrab, roma], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna] // since zurich is a parent +2 times, collapse last two
+         * 8: zagrab, [wein, roma], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna, zagrab]
+         * 9: wein, [warsawa, roma], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna, zagrab, wein]
+         * 10: warsawa, [wilno, roma], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna, zagrab, wein, warsawa]
+         * 11: wilno, [smolersk, roma], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna, zagrab, wein, warsawa, wilno]
+         * 12: smolersk, [moskva, roma], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna, zagrab, wein, warsawa, wilno, smolersk]
+         * 13: moskva, [roma], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna, zagrab:16, wein:14, warsawa:12, wilno:8, smolersk:5, moskva:2]
+         * 14: roma, [paleredo, brindisi], [zurich:10, charseille:8, barcelona:6, madrid:2], [zurich, vienna, zagrab:16, wein:14, warsawa:12, wilno:8, smolersk:5, moskva:2], [vienna, roma]
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
 
         return [path, length];
     }
