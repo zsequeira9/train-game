d29bb44dde7d5d23391b857b5ae6077e914e338b
unknown <zsequeira9@gmail.com>
2024-06-05 12:17:11 -0400

    move to its own component

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,20 +1,13 @@
 import { useState } from "react";
 
 import GameCanvas from "./GameCanvas";
+import GameConfigDialogue from "./GameConfigDialogue";
 import { USConfig } from "../config/US";
 import { PlayerColor } from "../types/interfaces";
 
 import { initControllerMachine } from "../state/initController";
 
 export default function App() {
-    let playerColorList = [
-        PlayerColor.RED,
-        PlayerColor.GREEN,
-        PlayerColor.BLUE,
-        PlayerColor.YELLOW,
-        PlayerColor.BLACK,
-        PlayerColor.PURPLE
-    ] as PlayerColor[];
 
     const [setup, setSetup] = useState<boolean>(true);
 
@@ -28,42 +21,16 @@ export default function App() {
 
     const controller = initControllerMachine(config, players, debug, startingTrains)
 
-    const debugConfigDialogue = <div>
-        <label>
-            Number trains: <input name="startingTrains" 
-                type="number" 
-                value={startingTrains}
-                onChange={e => setStartingTrains(Number(e.target.value))}/>
-        </label>
-    </div>
-
-    const gameConfigDialogue = <div className="destination-popup-wrapper">
-        <div className="destination-popup">
-            <label>
-                Players: <input name="names"
-                    onChange={e => {
-                        let names = e.target.value.split(",");
-                        setPlayers(names.map((name, i) => [name, playerColorList[i]]))
-                    }}
-                    type="text" />
-            </label>
-            <label>
-                Debug? <input name="debug"
-                    value={debug ? 1 : 0}
-                    onChange={e => setDebug(e.target.checked)}
-                    type="checkbox" />
-            </label>
-            {debug && debugConfigDialogue}
-            <button onClick={() => setSetup(false)}>
-                Start?
-            </button>
-        </div>
-    </div>
-
     return (
         <main>
             {setup ? 
-            gameConfigDialogue : 
+            <GameConfigDialogue 
+                startingTrains={startingTrains}
+                setStartingTrains={setStartingTrains}
+                setPlayers={setPlayers}
+                debug={debug}
+                setDebug={setDebug}
+                setSetup={setSetup}/> : 
             <GameCanvas config={config} controller={controller} />}
         </main>
     )
